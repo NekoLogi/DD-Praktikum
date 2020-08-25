8c73804fce70a744b131b542af69dd2cbe6d305e
NekoLogi <NekoLogi@github.com>
2020-08-25 08:20:58 +0200

    update

--- DD-Praktikum/src/org/praktikum/tests/Test_7.java
+++ DD-Praktikum/src/org/praktikum/tests/Test_7.java
@@ -18,38 +18,45 @@ public class Test_7 {
 		List warePrice = new List();
 		List wareAmount = new List();
 		
-		System.out.println("Brieftaschen Inhalt: ");
-		sMoney = user.nextLine();
-		int money = Integer.parseInt(sMoney);
-		
-		while (choice == "1") {
-			System.out.println("Warenname: ");
-			wareName.add(user.nextLine());
+		try {
+			System.out.println("Brieftaschen Inhalt: ");
+			sMoney = user.nextLine();
+			int money = Integer.parseInt(sMoney);
 			
-			System.out.println("Preis: ");
-			warePrice.add(user.nextLine());
-			
-			System.out.println("Wie viel von dieser Ware? ");
-			String s = user.nextLine();
-			wareAmount.add(s);
-			
-			System.out.println("Willst du noch eine Ware einfügen? 1 = ja | 2 = nein");
-			choice = user.nextLine();
-		}
-		user.close();
+			while (choice == "1") {
+				System.out.println("Warenname: ");
+				wareName.add(user.nextLine());
+				
+				System.out.println("Preis: ");
+				warePrice.add(user.nextLine());
+				
+				System.out.println("Wie viel von dieser Ware? ");
+				String s = user.nextLine();
+				wareAmount.add(s);
+				
+				System.out.println("Willst du noch eine Ware einfügen? 1 = ja | 2 = nein");
+				choice = user.nextLine();
+			}
+			user.close();
 
-		System.out.println("-------------------------------");
-		for (int i = 0; i < wareName.getItemCount(); i++) {
+			System.out.println("-------------------------------");
+			for (int i = 0; i < wareName.getItemCount(); i++) {
+				
+				System.out.println(wareName.getItem(i)
+						+ "\tx" + wareAmount.getItem(i) + " " 
+						+ (offMoney = (Integer.parseInt(warePrice.getItem(i)) * Integer.parseInt(wareAmount.getItem(i)))) 
+						+ "€");
+				money -= offMoney;
+			}
+			System.out.println("-------------------------------");
 			
-			System.out.println(wareName.getItem(i) + "\tx" + wareAmount.getItem(i) + " " + (offMoney = (Integer.parseInt(warePrice.getItem(i)) * Integer.parseInt(wareAmount.getItem(i)))) + "€");
-			money -= offMoney;
+			if(money < 0) {
+				System.out.print("Dein Geld reicht nicht aus dir fehlen noch: " + money + '€');
+			} else {
+				System.out.println(money + '€');
+			}
+		} catch (Exception e){
+			System.out.println("Etwas ist schief gelaufen: " + e.toString());
 		}
-		System.out.println("-------------------------------");
-		
-		if(money < 0) {
-			String stringMoney = Integer.toString(money).trim("-");
-			System.out.print("Dein Geld reicht nicht aus dir fehlen noch: " + stringMoney);
-		}
-		System.out.println(money + "€");
 	}
 }
