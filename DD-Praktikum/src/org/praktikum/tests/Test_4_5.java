5fa5f78d5ae0b3390ae6cea8c116e15e8d4a9aed
NekoLogi <NekoLogi@github.com>
2020-08-26 09:31:12 +0200

    Update

--- DD-Praktikum/src/org/praktikum/tests/Test_4_5.java
+++ DD-Praktikum/src/org/praktikum/tests/Test_4_5.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test_4_5 {
+	
+	public static ArrayList<Character> romInput = new ArrayList<Character>();
+	
+	public static String romZiffer = "IVXLCDM";
+	public static int[] dezZiffer = {1, 5, 10, 50, 100, 500, 1000};
 
 	public static void main(String[] args) {
 		
@@ -16,18 +21,14 @@ public class Test_4_5 {
 	
 	public static void FromRomanNummerals(Scanner user) {
 		
-		ArrayList<Character> romInput = new ArrayList<Character>();
-		
 		int summe = 0;
 		int firstZiff = 0;
 		int secZiff = 0;
-		
-		String romZiffer = "IVXLCDM";
-		int[] dezZiffer = {1, 5, 10, 50, 100, 500, 1000};
 				
 		System.out.println("Römische Ziffern:");
 		String input = user.nextLine();
 		user.close();
+		
 		for (int i = 0; i < input.length(); i++) {
 			romInput.add(input.charAt(i));
 		}
@@ -52,48 +53,18 @@ public class Test_4_5 {
 	
 	public static void ToRomanNummerals(Scanner user) {
 		
-		List newZiffer = new List();
-		
-		String[] romZiffer = {"I", "V", "X", "L", "C", "D", "M"};
-		
-		System.out.println("Dezimal Ziffer:");
+		System.out.println("Römische Ziffern:");
 		int input = Integer.parseInt(user.nextLine());
+		user.close();
 		
-		
-		while (input != 0) {
-			if (input < 0) {
-				input = 0;
-			}
-			if (input >= 1000) {
-				newZiffer.add(romZiffer[6].toString());
-				input -= 1000;
-			} else if (input < 1000 && input > 499) {
-				newZiffer.add(romZiffer[5].toString());
-				input -= 500;
-			} else if (input < 500 && input > 99) {
-				newZiffer.add(romZiffer[4].toString());
-				input -= 100;
-			} else if (input < 100 && input > 49) {
-				newZiffer.add(romZiffer[3].toString());
-				input -= 50;
-			} else if (input < 50 && input > 9) {
-				newZiffer.add(romZiffer[2].toString());
-				input -= 10;
-			} else if (input < 10 && input > 4) {
-				newZiffer.add(romZiffer[1].toString());
-				input -= 5;
-			} else if (input < 5 && input == 4) {
-				newZiffer.add(romZiffer[0].toString());
-				newZiffer.add(romZiffer[1].toString());
-				input -= 4;
-			} else if (input < 4 && input != 0) {
-				newZiffer.add(romZiffer[0].toString());
-				input -= 1;
+		for (int i = 6; i < 0; i--) {
+			while(input > dezZiffer[i]) {
+				input -= dezZiffer[i];
+				romInput.add(romZiffer.charAt(i));
 			}
 		}
-		System.out.print("Ziffern: ");
-		for (int i = 0; i < newZiffer.getItemCount(); i++) {
-			System.out.print(newZiffer.getItem(i));
+		for (int i = 0; i < romInput.size(); i++) {
+			System.out.print(romInput.get(i));
 		}
 	}
 }
