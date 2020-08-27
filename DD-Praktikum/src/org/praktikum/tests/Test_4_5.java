6fdc996f90c872e5324cd753c9f9c4a2a42e6993
Michaelsys <70153642+Michaelsys@users.noreply.github.com>
2020-08-27 18:19:40 +0200

    Update
    römische zahlen fertig
    followup email fertig

--- DD-Praktikum/src/org/praktikum/tests/Test_4_5.java
+++ DD-Praktikum/src/org/praktikum/tests/Test_4_5.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 
 public class Test_4_5 {
 	
-	public static ArrayList<Character> romInput = new ArrayList<Character>();
-	
 	public static String romZiffer = "IVXLCDM";
 	public static int[] dezZiffer = {1, 5, 10, 50, 100, 500, 1000, 0};
+	static int charCount = 1;
+	static int input = 0;
+	static int index = 0;
+	static int countCharCount = 0;
+	
+	public static ArrayList<Character> romInput = new ArrayList<Character>();
+	static ArrayList<Character> newOutput = new ArrayList<Character>();
 
+	
 	public static void main(String[] args) {
 		
 		Scanner user = new Scanner(System.in);
@@ -20,57 +26,54 @@ public class Test_4_5 {
 	
 	public static void FromRomanNummerals(Scanner user) {
 		
-		int summe = 0;
-		int firstZiff = 0;
-		int secZiff = 0;
-				
 		System.out.println("Römische Ziffern:");
-		String input = user.nextLine();
+		input = Integer.parseInt(user.nextLine());
 		user.close();
 		
-		for (int i = 0; i < input.length(); i++) {
-			romInput.add(input.charAt(i));
-		}
-		for (int i = 0; i < input.length(); i++) {
-			try {
-				char[] ziffer = {romInput.get(i), romInput.get(i + 1)};
-				firstZiff = romZiffer.lastIndexOf(ziffer[0]);
-				secZiff = romZiffer.lastIndexOf(ziffer[1]);
-				
-				if (firstZiff < secZiff) {
-					summe += (dezZiffer[secZiff] - dezZiffer[firstZiff]);
-				} else if (firstZiff >= secZiff) {
-					summe += (dezZiffer[firstZiff] + dezZiffer[secZiff]);
+		GetRomanNummerals();
+		
+		try {
+			for (index = index; index < romInput.size(); index++) {
+				if (romInput.get(index) == romInput.get(index + 1)) {
+					charCount++;
+					if (charCount > 3) {
+						countCharCount++;
+					}
+				} else if (index + 1 >= romInput.size()) {
+					break;
+					} else if(charCount > 3) {
+					int n = romZiffer.indexOf(romInput.get(index));
+					
+					ProcessNummerals(n);
+					//index = index + 1;
+					charCount = 1;
+					countCharCount++;
+				} else {
+					newOutput.add(romInput.get(index));
 				}
-				i++;
-			} catch(Exception err) {
-				summe += dezZiffer[firstZiff];
 			}
-		}
-		System.out.println(summe);
-	}
-	
-	public static void GetRomanNummerals(int input) {
-		for (int i = 6; i > -1; i--) {
-			while(input >= dezZiffer[i]) {
-				input -= dezZiffer[i];
-				romInput.add(romZiffer.charAt(i));
-			}
+
+			
+		} catch (Exception err) { }	
+		try {
+			EndResult();
+		} catch (Exception err) {
+			EndResult();
 		}
 	}
 	
 	public static void ToRomanNummerals(Scanner user) {
-			
-		int test = 42;
-		int input = 42;
+		
 		int charCount = 1;
 		int e = 6;
 		
 		ArrayList<Character> newOutput = new ArrayList<Character>();
 		
-		//System.out.println("Römische Ziffern:");
-		//int input = Integer.parseInt(user.nextLine());
-		//user.close();
+		System.out.println("Römische Ziffern:");
+		input = Integer.parseInt(user.nextLine());
+		user.close();
+		
+		int test = input;
 		
 		GetRomanNummerals(input);
 		
@@ -110,4 +113,67 @@ public class Test_4_5 {
 		}
 
 	}
+	
+	public static void GetRomanNummerals() {
+
+		for (int i = 6; i > -1; i--) {
+			while(input >= dezZiffer[i]) {
+				input -= dezZiffer[i];
+				romInput.add(romZiffer.charAt(i));
+			}
+		}
+		
+		for (int i = 0; i < romInput.size(); i++) {
+			System.out.print(romInput.get(i));
+		}
+		System.out.println("");
+	}
+	
+	public static void ProcessNummerals(int n) {
+
+		if(dezZiffer[n] < dezZiffer[n + 1]) {			
+			int restSumme = dezZiffer[n + 1] - (dezZiffer[n] * charCount);	
+			while (restSumme != 0) {
+				for (int i = 6; i > -1; i--) {
+					if (restSumme >= dezZiffer[i]) {
+						restSumme = restSumme - dezZiffer[i];						
+						newOutput.add(romZiffer.charAt(i));
+						newOutput.add(romZiffer.charAt(n + 1));
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	public static void EndResult() {
+		if (charCount > 3) {
+			int n = romZiffer.indexOf(romInput.get(index));
+			
+			ProcessNummerals(n);
+			charCount = 1;
+		} else {
+			newOutput.add(romInput.get(index - 1));
+			
+			for (index = index; index < romInput.size(); index++) {
+				newOutput.add(romInput.get(index));
+			}
+		}
+		if (countCharCount != 0) {
+			for (int i = 0; i < newOutput.size(); i++) {
+				System.out.print(newOutput.get(i));
+			}
+		}
+		
+	}
+	
+	public static void GetRomanNummerals(int input) {
+		for (int i = 6; i > -1; i--) {
+			while(input >= dezZiffer[i]) {
+				input -= dezZiffer[i];
+				romInput.add(romZiffer.charAt(i));
+			}
+		}
+	}
+	
 }
--- DD-Praktikum/src/org/praktikum/tests/Test_6.java
+++ DD-Praktikum/src/org/praktikum/tests/Test_6.java
@@ -1,49 +1,149 @@
 package org.praktikum.tests;
 
-import java.awt.List;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Test_6 {
-
+	
+	static LocalDate dateNow = LocalDate.now();
+	static LocalTime timeNow = LocalTime.now();
+	
+	static boolean isOffTime;
+	
+	static String[] dateTime = {"[!]", "month", "year", "hour", "minute"};
+	static String[] dateMonth = {"jan", "feb", "mar", "apr", "mai", "jun", "jul", "aug", "sep", "okt", "nov", "dez"};
+	static char[] dateDays = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	static char[] dateTimeChar = { '!' , 'm' , 'y'};
+	
+	static int[] storeDateTime = new int[5];
+	static int[] localeDate = {dateNow.getDayOfMonth(), dateNow.getMonthValue(), dateNow.getYear(),
+			timeNow.getHour(), timeNow.getMinute()};
+	
+	
 	public static void main(String[] args) {
 		
 		Scanner user = new Scanner(System.in);
 		
-		LocalDate dateNow = LocalDate.now();
-		LocalTime timeNow = LocalTime.now();
+		String input = "aug20-9am@followup.cc";
+		String[] eMail = input.split("@");
 		
-		List mailDate = new List();
-		List mailTime = new List();
+		GetMailTime(eMail);
 		
-		String years = "";
-		String months = "";
-		String weeks = "";
-		String days = "";
-		String hours = "";
-		
-		String[] stringTime = timeNow.toString().split("[:.]");
-		String[] stringDate = dateNow.toString().split("[-]");
+		ProcessMailTime();
+	}
 	
-		System.out.println("eingabe: ");
-		String input = user.nextLine();
+	public static void GetMailTime(String[] eMail) {
+
+// Offset Time.
+		for (int i = 0; i < 10; i++) {
+			if (eMail[0].contains(Character.toString(dateDays[i]))) {
+				OffsetTime(eMail);
+				break;
+			}
+		}
+// Set time.
+		for (int i = 0; i < 12; i++) {
+			if (eMail[0].contains(dateMonth[i])) {
+				SetTime(eMail, i);
+				break;
+			}
+		}
+	}
+	
+	public static void ProcessMailTime() {
 		
-		if (input.contains("hour") || input.contains("hours")) {
-			hours = input.replace("hours", "");
+		Calendar cal = Calendar.getInstance();
+		
+		
+		if (isOffTime == true) {
+			Date date = new Date(System.currentTimeMillis());
+			cal.setTime(date);
+			
+			cal.add(Calendar.HOUR_OF_DAY, storeDateTime[3]);
+			cal.add(Calendar.DAY_OF_MONTH, storeDateTime[0]);
+			cal.add(Calendar.MONTH, storeDateTime[1]);
+			cal.add(Calendar.YEAR, storeDateTime[2]);
+			cal.add(Calendar.MINUTE, storeDateTime[4]);
+		} else {
+			cal.set(Calendar.HOUR, storeDateTime[3]);
+			cal.set(Calendar.AM_PM, storeDateTime[3]);
+			cal.set(Calendar.MONTH, storeDateTime[1]);
+			cal.set(Calendar.DAY_OF_MONTH, storeDateTime[0] - 4);
+			cal.set(Calendar.YEAR, localeDate[2]);
+			cal.set(Calendar.MINUTE, 0);
 		}
-		if (input.contains("day") || input.contains("Days")) {
-			days = input.replace("days", "");
+
+		SimpleDateFormat dateForm = new SimpleDateFormat("yyyy-MM-dd hh:mm aa");
+		System.out.println(dateForm.format(cal.getTime()));
+	}
+	
+	public static void OffsetTime(String[] eMail) {
+		int index = 0;
+		
+		isOffTime = true;
+		
+		if(eMail[0].contains("week")) {
+			index = eMail[0].indexOf('w');
+			storeDateTime[0] = 7 * Integer.parseInt(Character.toString(eMail[0].charAt(index - 1)));
+			
+			if(eMail[0].contains("day")) {
+				index = eMail[0].indexOf('d');
+				storeDateTime[0] = storeDateTime[0] + Integer.parseInt(Character.toString(eMail[0].charAt(index - 1)));
+			}
 		}
-		if (input.contains("week") || input.contains("weeks")) {
-			weeks = input.replace("weeks", "");
+		
+		for (int i = 0; i < 3; i++) {	
+			if (eMail[0].contains(dateTime[i])) {
+				index = eMail[0].indexOf(dateTimeChar[i]);
+				storeDateTime[i] = Integer.parseInt(Character.toString(eMail[0].charAt(index - 1)));
+			}
 		}
-		if (input.contains("month") || input.contains("months")) {
-			months = input.replace("months", "");
+		
+		if (eMail[0].contains("hour")) {
+			index = eMail[0].indexOf('h');
+			storeDateTime[3] = Integer.parseInt(Character.toString(eMail[0].charAt(index - 1)));
 		}
-		if (input.contains("year") || input.contains("years")) {
-			years = input.replace("years", "");
+		if (eMail[0].contains("minute")) {
+			index = eMail[0].indexOf('n') - 2;
+			storeDateTime[4] = Integer.parseInt(Character.toString(eMail[0].charAt(index - 1)));
 		}
-		System.out.print(hours + days + weeks);
+	}
+
+	public static void SetTime(String[] eMail, int i) {
+		isOffTime = false;
+		
+		storeDateTime[1] = i + 1;
+		for (int e = 0; e < 10; e++) {
+			if (eMail[0].charAt(4) == dateDays[e]) {
+				String cache = Character.toString(eMail[0].charAt(3)) + "" + e;
+				storeDateTime[0] = Integer.parseInt(cache);
+				break;
+			} else {
+				storeDateTime[0] = Integer.parseInt(Character.toString(eMail[0].charAt(3)));
+			}
+		}
+		
+		for (int j = 0; j < 10; j++) {
+			if (eMail[0].charAt(8) == dateDays[j]) {
+				String cache = Character.toString(eMail[0].charAt(8)) + "" + (j + 1);
+				int aMPM = AmPm(eMail);
+				storeDateTime[3] = Integer.parseInt(cache) + aMPM;
+			} else {
+				int aMPM = AmPm(eMail);
+				storeDateTime[3] = Integer.parseInt(Character.toString(eMail[0].charAt(6))) + aMPM;
+			}
+		}
+	}
+
+	public static int AmPm(String[] eMail) {
+		if (eMail[0].contains("am")) {
+			return 0;
+		} else {
+			return 12;
+		}
 	}
 }
